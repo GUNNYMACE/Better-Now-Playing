95af2a4c6d60fa144820f21cde902d3a0e1abf42
GUNNYMACE <87084580+GUNNYMACE@users.noreply.github.com>
2025-01-04 19:23:17 -0600

    Updated Spotify Support
    Quick back-up of the current state of the app 1/4/25

---
+++ Better Now Playing/Classes/Spotify/SpotifyManager.swift
@@ -0,0 +1,122 @@
+//
+//  SpotifyManager.swift
+//  Better Now Playing
+//
+//  Created by Mason Likens on 1/2/25.
+//
+
+import UIKit
+import WebKit
+
+class SpotifyManager: UIViewController, SPTSessionManagerDelegate, SPTAppRemoteDelegate {
+    
+    // MARK: - Spotify Authorization & Configuration
+    var responseCode: String? {
+        didSet {
+            fetchAccessToken { (dictionary, error) in
+                if let error = error {
+                    print("Fetching token request error \(error)")
+                    return
+                }
+                let accessToken = dictionary!["access_token"] as! String
+                DispatchQueue.main.async {
+                    self.appRemote.connectionParameters.accessToken = accessToken
+                    self.appRemote.connect()
+                }
+            }
+        }
+    }
+    
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+    
+    var accessToken = UserDefaults.standard.string(forKey: accessTokenKey) {
+        didSet {
+            let defaults = UserDefaults.standard
+            defaults.set(accessToken, forKey: accessTokenKey)
+        }
+    }
+    
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: spotifyClientId, redirectURL: redirectUri)
+        configuration.playURI = ""
+        configuration.tokenSwapURL = URL(string: "http://localhost:1234/swap")
+        configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
+        return configuration
+    }()
+
+    lazy var sessionManager: SPTSessionManager? = {
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
+    
+    func attemptAuth() {
+        guard let sessionManager = sessionManager else { return }
+        sessionManager.initiateSession(with: scopes, options: .clientOnly, campaign: "")
+        
+        print(accessToken)
+    }
+    
+    //MARK: Required Methods
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("session initiated")
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: any Error) {
+        print("session failed")
+    }
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("Established connection: app")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: (any Error)?) {
+        print("Failed Connection: app")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: (any Error)?) {
+        print("Disconected: app")
+    }
+}
+
+// MARK: - Networking
+extension SpotifyManager {
+    func fetchAccessToken(completion: @escaping ([String: Any]?, Error?) -> Void) {
+        let url = URL(string: "https://accounts.spotify.com/api/token")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let spotifyAuthKey = "Basic \((spotifyClientId + ":" + spotifyClientSecretKey).data(using: .utf8)!.base64EncodedString())"
+        request.allHTTPHeaderFields = ["Authorization": spotifyAuthKey, "Content-Type": "application/x-www-form-urlencoded"]
+
+        var requestBodyComponents = URLComponents()
+        let scopeAsString = stringScopes.joined(separator: " ")
+
+        requestBodyComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: spotifyClientId),
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: responseCode!),
+            URLQueryItem(name: "redirect_uri", value: redirectUri.absoluteString),
+            URLQueryItem(name: "code_verifier", value: ""), // not currently used
+            URLQueryItem(name: "scope", value: scopeAsString),
+        ]
+
+        request.httpBody = requestBodyComponents.query?.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,                              // is there data
+                    let response = response as? HTTPURLResponse,  // is there HTTP response
+                    (200 ..< 300) ~= response.statusCode,         // is statusCode 2XX
+                    error == nil else {                           // was there no error, otherwise ...
+                    print("Error fetching token \(error?.localizedDescription ?? "")")
+                    return completion(nil, error)
+            }
+            
+            let responseObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+            print("Access Token Dictionary=", responseObject ?? "")
+            completion(responseObject, nil)
+        }
+        task.resume()
+    }
+}
